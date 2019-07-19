ec9723069e7551f51f2c1590261934530d748c48
VampireVol <sergeyb11@ya.ru>
2019-07-19 16:41:40 +0300

    Add Complex

---
+++ Complex/Complex/ComplexNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class ComplexNumber
+    {
+        private int _real;
+        private int _image;
+
+        public ComplexNumber(int real = 0, int image = 0)
+        {
+            _real = real;
+            _image = image;
+        }
+
+        public static ComplexNumber operator +(ComplexNumber left, ComplexNumber right)
+        {
+            return new ComplexNumber(left._real + right._real, left._image + right._image);
+        }
+
+        public static ComplexNumber operator -(ComplexNumber left, ComplexNumber right)
+        {
+            return new ComplexNumber(left._real - right._real, left._image - right._image);
+        }
+
+        public static ComplexNumber operator *(ComplexNumber left, ComplexNumber right)
+        {
+            return new ComplexNumber(left._real * right._real - left._image * right._image, left._real * right._image + left._image * right._real);
+        }
+
+        public override string ToString()
+        {
+            string sign;
+            if (_image < 0)
+                sign = "-";
+            else
+                sign = "+";
+            return _real + " " + sign + " i" + Math.Abs(_image);
+        }
+    }
+}
---
+++ Complex/Complex/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int realFirst, realSecond, imageFirst, imageSecond;
+            Console.Write ("Введите вещественную часть 1 числа: ");
+            realFirst = int.Parse(Console.ReadLine());
+            Console.Write("Введите мнимую часть 1 числа: ");
+            imageFirst = int.Parse(Console.ReadLine());
+            Console.Write("Введите вещественную часть 2 числа: ");
+            realSecond = int.Parse(Console.ReadLine());
+            Console.Write("Введите мнимую часть 2 числа: ");
+            imageSecond = int.Parse(Console.ReadLine());
+
+            ComplexNumber first = new ComplexNumber(realFirst, imageFirst);
+            ComplexNumber second = new ComplexNumber(realSecond, imageSecond);
+            Console.WriteLine("Сумма чисел: {0}", first + second);
+            Console.WriteLine("Разность чисел: {0}", first - second);
+            Console.WriteLine("Произведение чисел: {0}", first * second);
+            Console.ReadKey();
+        }
+    }
+
+    
+}
