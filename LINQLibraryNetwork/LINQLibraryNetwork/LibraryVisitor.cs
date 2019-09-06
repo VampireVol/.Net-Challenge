26ac91b07690c57d8acf59a09563e945a8dc88d2
VampireVol <sergeyb11@ya.ru>
2019-09-06 17:38:34 +0300

    LINQLibraryNetwork fix

--- LINQLibraryNetwork/LINQLibraryNetwork/LibraryVisitor.cs
+++ LINQLibraryNetwork/LINQLibraryNetwork/LibraryVisitor.cs
@@ -8,24 +8,5 @@ namespace LINQLibraryNetwork
 
         public Guid VisitorId { get; }
 
-        public Guid CopyBookId { get; }
-
-        public DateTime Take { get; }
-
-        public DateTime? Return
-        {
-            get
-            {
-                if (Return == null)
-                {
-                    Console.WriteLine();
-                    return null;
-                }
-                else
-                {
-                    return Return;
-                }
-            }
-        }
     }
 }
--- LINQLibraryNetwork/LINQLibraryNetwork/Log.cs
+++ LINQLibraryNetwork/LINQLibraryNetwork/Log.cs
@@ -4,6 +4,29 @@ namespace LINQLibraryNetwork
 {
     public class Log
     {
-        public Guid 
+        public Guid LibraryId { get; }
+
+        public Guid VisitorId { get; }
+
+        public Guid CopyBookId { get; }
+
+        public DateTime Take { get; }
+
+        public DateTime? Return
+        {
+            get
+            {
+                if (Return == null)
+                {
+                    Console.WriteLine();
+                    return null;
+                }
+                else
+                {
+                    return Return;
+                }
+            }
+        }
     }
+}
 }
