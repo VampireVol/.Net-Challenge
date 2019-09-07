212d0514d75a9f8bc8088b2add660efc701f135f
VampireVol <sergeyb11@ya.ru>
2019-09-07 14:29:07 +0300

    LINQLibraryNetwork 3 request add

--- LINQLibraryNetwork/LINQLibraryNetwork/Program.cs
+++ LINQLibraryNetwork/LINQLibraryNetwork/Program.cs
@@ -37,7 +37,7 @@ namespace LINQLibraryNetwork
             {
                 new Library("Центральная библиотека Нижне Верховья", "Ул. Пирожкова, 3а", districts[2].Id),
                 new Library("Детская библиотека №1", "Ул. Петрушкина, 40", districts[3].Id),
-                new Library("Облостная библиотека", "Ул. Пролетарская, 22", districts[3].Id),
+                new Library("Областная библиотека", "Ул. Пролетарская, 22", districts[3].Id),
                 new Library("Городская библиотека №5", "Ул. Испытаний, 13", districts[3].Id),
                 new Library("Городская библиотека №2", "Ул. Мира, 49", districts[1].Id),
                 new Library("Детская библиотека №3", "Ул. Предприятий, 11", districts[1].Id),
@@ -73,6 +73,44 @@ namespace LINQLibraryNetwork
                 new CopyBook(RelaseForm.Soft, books[5].Id),
                 new CopyBook(RelaseForm.Solid, books[5].Id),
             };
+
+            var libNames = from lib in libraries
+                select  lib.Name;
+
+            foreach (var libName in libNames)
+            {
+                Console.WriteLine(libName);
+            }
+
+            Console.WriteLine();
+
+            var libNamesGroup = from lib in libraries
+                                join dis in districts on lib.DistrictId equals dis.Id
+                                group lib by dis.Name;
+
+            foreach (var libNameGroup in libNamesGroup)
+            {
+                Console.WriteLine($"{libNameGroup.Key} ({libNameGroup.Count()})");
+                foreach (var library in libNameGroup)
+                {
+                    Console.WriteLine(library.Name);
+                }
+            }
+
+            Console.WriteLine();
+
+            var withLibDistricts = (from dis in districts
+                                    join lib in libraries on dis.Id equals lib.DistrictId
+                                    select dis).ToList();
+            var withoutLibDistricts = districts.Except(withLibDistricts);
+
+            foreach (var withoutLibDistrict in withoutLibDistricts)
+            {
+                Console.WriteLine(withoutLibDistrict.Name);
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
